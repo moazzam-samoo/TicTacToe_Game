0af1b6e663d1773e55cc8364d5ca8e46bc94fe5d
moazzam-samoo <moazzamsamoo819@gmail.com>
2024-05-09 23:42:40 +0500

    Added tic tac toe constractrator and setting up the frame

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -4,5 +4,17 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class TicTacToe {
+    int boardheight = 600;
+    int boardwidth = 650;
+    //Mehak Commit (place)
+    JFrame frame = new JFrame("Tic Tac Toe");
 
+    TicTacToe(){
+        frame.setVisible(true);
+        frame.setSize(boardwidth,boardheight);
+        frame.setLocationRelativeTo(null); // Opening location on Center of Screen
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// For Exiting Program when you click on X on the top right
+        frame.setLayout(new BorderLayout());
+    }
 }
