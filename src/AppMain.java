7fa5b4483955cabbf7233ea65a58620875fa1e25
moazzam-samoo <moazzamsamoo819@gmail.com>
2024-05-09 22:25:50 +0500

    Uploaded Project

---
+++ src/AppMain.java
@@ -0,0 +1,6 @@
+public class AppMain {
+    public static void main(String[] args) {
+        //Object of the TicTacToe Class for Fetching Data.
+        TicTacToe ticTacToe = new TicTacToe();
+    }
+}
---
+++ src/TicTacToe.java
@@ -0,0 +1,8 @@
+//Imported Packages that is used on Game.
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class TicTacToe {
+
+}
