7937847742c4737a7c6e486bb95b3781cf4a3cf4
moazzam-samoo <moazzamsamoo819@gmail.com>
2024-05-09 23:59:45 +0500

    Fixed Error that are pushed by Hussnain.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -16,24 +16,6 @@ public class TicTacToe {
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// For Exiting Program when you click on X on the top right
         frame.setLayout(new BorderLayout());
-        
-        // mehak (1)
-
-
-
-
-        // mehak (1) end
-
-
-        
-        textpanel.setLayout(new BorderLayout());
-        textpanel.add(textlabel);
-        frame.add(textpanel, BorderLayout.NORTH);
-    
-        boardpanel.setLayout(new GridLayout(3, 3)); // Creates 3 By 3 Layout
-        boardpanel.setBackground(Color.black);
-        frame.add(boardpanel);
-
 
    
     }
