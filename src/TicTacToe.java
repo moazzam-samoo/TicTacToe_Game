0f47d1425f148d2a17743856d0b22c9bfc78ae12
moazzam-samoo <moazzamsamoo819@gmail.com>
2024-05-12 01:10:38 +0500

    Added Set Tie Function to check is game is TIE? and also fixed the bugs

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,18 +1,18 @@
 //Imported Packages that is used on Game.
-import java.awt.*;
-import java.awt.event.*;
+
 import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
 
 public class TicTacToe {
     int boardheight = 600;
     int boardwidth = 650;
-    //Mehak Commit (place)
     JFrame frame = new JFrame("Tic Tac Toe");
     JLabel textlabel = new JLabel();
     JPanel textpanel = new JPanel();
     JPanel boardpanel = new JPanel();
 
-    JButton board[][] = new JButton[3][3];
+    JButton[][] board = new JButton[3][3];
 
     String playerX = "X";
     String playerO = "O";
@@ -58,30 +58,28 @@ public class TicTacToe {
                 tile.setFocusable(false);
 
                 // Clicking the Tile sets text of Current Player so either  ( X or O )
-                tile.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e){
-                        JButton tile =(JButton) e.getSource();
-                        if (gameOver) return;
+                tile.addActionListener(e -> {
+                    JButton tile1 =(JButton) e.getSource();
+                    if (gameOver) return;
 
-                        // Makes it so You can't overwrie on a tile
-                        if (tile.getText()== "") {
+                    // Makes it so You can't overwrie on a tile
+                    if (Objects.equals(tile1.getText(), "")) {
 
-                            tile.setText(currentPlayer);
+                        tile1.setText(currentPlayer);
 
-                            turns++;
-              checkWinner();
+                        turns++;
+          checkWinner();
 
-                            // Shows who's turn is it
-                            if (!gameOver) {
-                                currentPlayer = currentPlayer == playerO ? playerX : playerO;
-                                textlabel.setText(currentPlayer+ "'s turn.");
-                            }
-
-
+                        // Shows who's turn is it
+                        if (!gameOver) {
+                            currentPlayer = Objects.equals(currentPlayer, playerO) ? playerX : playerO;
+                            textlabel.setText(currentPlayer+ "'s turn.");
                         }
 
 
                     }
+
+
                 });
 
             }
@@ -92,10 +90,10 @@ public class TicTacToe {
         void checkWinner () {
             // Horizontal
          for (int r = 0; r < 3; r++) {
-                if (board[r][0].getText() == "") continue;
+                if (Objects.equals(board[r][0].getText(), "")) continue;
 
-                if (board[r][0].getText() == board[r][1].getText() &&
-                        board[r][1].getText() == board[r][2].getText()) {
+                if (Objects.equals(board[r][0].getText(), board[r][1].getText()) &&
+                        Objects.equals(board[r][1].getText(), board[r][2].getText())) {
 
                     for(int i = 0; i < 3 ; i++){
                         setWinner(board[r][i]);
@@ -110,10 +108,10 @@ public class TicTacToe {
 
             //Vertical
         for (int c = 0; c < 3; c++) {
-            if (board[0][c].getText() == "") continue;
+            if (Objects.equals(board[0][c].getText(), "")) continue;
         
-            if (board[0][c].getText() == board[1][c].getText() &&
-                board[1][c].getText() == board[2][c].getText()) {
+            if (Objects.equals(board[0][c].getText(), board[1][c].getText()) &&
+                    Objects.equals(board[1][c].getText(), board[2][c].getText())) {
                 for (int i = 0; i < 3; i++) {
                     setWinner(board[i][c]);
                 }
@@ -122,9 +120,9 @@ public class TicTacToe {
             }
         }
             // Diagonally
-            if (board[0][0].getText() == board[1][1].getText() &&
-                    board[1][1].getText() == board[2][2].getText() &&
-                    board[0][0].getText() != "") {
+            if (Objects.equals(board[0][0].getText(), board[1][1].getText()) &&
+                    Objects.equals(board[1][1].getText(), board[2][2].getText()) &&
+                    !Objects.equals(board[0][0].getText(), "")) {
                 for (int i = 0; i < 3; i++) {
                     setWinner(board[i][i]);
                 }
@@ -133,16 +131,24 @@ public class TicTacToe {
             }
    
             // Anti Diagonally
-   if (board[0][2].getText() == board[1][1].getText() &&
-   board[1][1].getText() == board[2][0].getText() &&
-   board[0][2].getText() != "") {
+   if (Objects.equals(board[0][2].getText(), board[1][1].getText()) &&
+           Objects.equals(board[1][1].getText(), board[2][0].getText()) &&
+           !Objects.equals(board[0][2].getText(), "")) {
    setWinner(board[0][2]);
    setWinner(board[1][1]);
    setWinner(board[2][0]);
    gameOver = true;
    return;
             }
-        
+            if (turns==9) {
+                for(int r = 0; r < 3 ; r++){
+                    for(int c = 0; c < 3; c++){
+                        setTie(board[r][c]);
+                    }
+                }
+
+                gameOver = true;
+            }
         }
 
 
