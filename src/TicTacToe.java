f770d696656ec7ad77e778357dea608946b0e9f8
haidertalpur7@gamil.com <haidertalpur7@gmail.com>
2024-05-12 00:35:55 +0500

    added logic for vertical win and removed errors

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -69,7 +69,7 @@ public class TicTacToe {
                             tile.setText(currentPlayer);
 
                             turns++;
-//                    checkWinner();
+              checkWinner();
 
                             // Shows who's turn is it
                             if (!gameOver) {
@@ -87,32 +87,55 @@ public class TicTacToe {
             }
         }
 
+    }
+        
+        void checkWinner () {
+            // Horizontal
+         for (int r = 0; r < 3; r++) {
+                if (board[r][0].getText() == "") continue;
 
-        //check winner \\???
-//        void checkWinner () {
-//            // Horizontal
-//            for (int r = 0; r < 3; r++) {
-//                if (board[r][0].getText() == "") continue;
-//
-//                if (board[r][0].getText() == board[r][1].getText() &&
-//                        board[r][1].getText() == board[r][2].getText()) {
-//
-////                    for(int i = 0; i < 3 ; i++){
-////                        setWinner(board[r][i]);
-////                    }
-////
-//                    gameOver = true;
-//                    return;
-//
-//                }
-//            }
+                if (board[r][0].getText() == board[r][1].getText() &&
+                        board[r][1].getText() == board[r][2].getText()) {
 
-//        void setWinner(JButton tile){
-//            tile.setForeground(Color.green);
-//            tile.setBackground(Color.DARK_GRAY);
-//            textlabel.setText(currentPlayer + " is the Winner!");
-//        } //end mehak 5
+                    for(int i = 0; i < 3 ; i++){
+                        setWinner(board[r][i]);
+                    }
 
+                   gameOver = true;
+                    return;
 
+                }
+            }
+        
+        
+            //Vertical
+        for (int c = 0; c < 3; c++) {
+            if (board[0][c].getText() == "") continue;
+        
+            if (board[0][c].getText() == board[1][c].getText() &&
+                board[1][c].getText() == board[2][c].getText()) {
+                for (int i = 0; i < 3; i++) {
+                    setWinner(board[i][c]);
+                }
+                gameOver = true;
+                return;
+            }
         }
+        }
+
+        //Diagonally
+        // mehak 4
+
+
+        //end mehak4
+
+        
+      void setWinner(JButton tile){
+          tile.setForeground(Color.green);
+          tile.setBackground(Color.DARK_GRAY);
+         textlabel.setText(currentPlayer + " is the Winner!");
+        } 
+
+
+        
     }
